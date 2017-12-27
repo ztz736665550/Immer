82dabcd7408ba00eca270ca64dcaac396d77aae4
ztz <736665550@qq.com>
2017-12-27 18:33:48 +0800

    Initial commit
    immersive沉浸式

---
+++ app/src/main/java/com/example/ztz/immer/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.example.ztz.immer;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * 图片类型的沉浸式
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+}
